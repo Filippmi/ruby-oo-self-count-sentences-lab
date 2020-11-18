cd962214cbf0737f7d848a27de664c01760bafa7
Filipp Minayev <Filippmi@gmail.com>
2020-11-18 13:05:20 -0700

    commit tree cf8f8b7b4ea61e0fa733be577f5097fc5ec3073d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    counter = 0
+    self.split(/[.?!]+/).select do |sentence|
+      counter += 1
+    end
+    counter
   end
 end
